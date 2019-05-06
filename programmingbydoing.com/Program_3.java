4ed7d92b389bddaa5db297157664dbf8136f8e4d
anwari-fikri <anwari.fikri@gmail.com>
2019-05-07 02:18:52 +0800

    pbd 3-8

---
+++ programmingbydoing.com/Program_3.java
@@ -0,0 +1,9 @@
+/*An Important Message*/
+
+public class Program_3
+{
+  public static void main(String [] args)
+  {
+    System.out.println( "Mr. Mitchell is cool." );
+  }
+}
---
+++ programmingbydoing.com/Program_7.java
@@ -0,0 +1,15 @@
+/*Your Initials*/
+
+public class Program_7
+{
+  public static void main(String [] args)
+  {
+    System.out.println( "    A      F F F F F" );
+    System.out.println( "  A   A    F"         );
+    System.out.println( "A       A  F"         );
+    System.out.println( "A A A A A  F F F"     );
+    System.out.println( "A       A  F"         );
+    System.out.println( "A       A  F"         );
+    System.out.println( "A       A  F"         );
+  }
+}
