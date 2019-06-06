eaedf5ba42112fa2f46eb03233d0c23cadf5ed71
anwari-fikri <50336496+anwari-fikri@users.noreply.github.com>
2019-06-07 07:59:26 +0800

    Add files via upload

---
+++ Program_3.java
@@ -0,0 +1,7 @@
+public class Program_3
+{
+  public static void main(String [] args)
+  {
+    System.out.println( "Mr. Mitchell is cool." );
+  }
+}
---
+++ Program_7.java
@@ -0,0 +1,13 @@
+public class Program_7
+{
+  public static void main(String [] args)
+  {
+    System.out.println( "  A    FFFFF" );
+    System.out.println( " A A   F" );
+    System.out.println( "A   A  F" );
+    System.out.println( "AAAAA  FFF" );
+    System.out.println( "A   A  F" );
+    System.out.println( "A   A  F" );
+    System.out.println( "A   A  F" );
+  }
+}
